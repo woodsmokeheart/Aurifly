bea980d48b8460ec31fc9df9fce7d5842c557b77
Denis Kukobin <denis.kukobin@hipasus.com>
2025-03-05 11:24:38 +0300

    added like button for mini player

--- src/components/MiniPlayer/MiniPlayer.tsx
+++ src/components/MiniPlayer/MiniPlayer.tsx
@@ -1,40 +1,53 @@
-import React from "react";
-import { IoPlayCircle, IoPauseCircle } from "react-icons/io5";
+import React, { useState } from "react";
+import {IoPlayCircle, IoPauseCircle, IoHeart, IoHeartOutline} from "react-icons/io5";
 import css from "./MiniPlayer.module.css";
 
 interface Track {
-  id: number;
-  title: string;
-  artist: string;
-  poster: string;
-  audioUrl: string;
-  duration: string;
+    id: number;
+    title: string;
+    artist: string;
+    poster: string;
+    audioUrl: string;
+    duration: string;
 }
 
 interface MiniPlayerProps {
-  track: Track;
-  isPlaying: boolean;
-  onOpen: () => void;
+    track: Track;
+    isPlaying: boolean;
+    onOpen: () => void;
 }
 
-export const MiniPlayer: React.FC<MiniPlayerProps> = ({
-  track,
-  isPlaying,
-  onOpen,
-}) => {
-  return (
-    <div className={css.miniPlayer} onClick={onOpen}>
-      <img src={track.poster} alt={track.title} className={css.poster} />
-      <div className={css.marquee}>
-        <span>{`${track.title} - ${track.artist}`}</span>
-      </div>
-      <div className={css.playButton}>
-        {isPlaying ? (
-          <IoPauseCircle size={32} className={css.playIcon} />
-        ) : (
-          <IoPlayCircle size={32} className={css.playIcon} />
-        )}
-      </div>
-    </div>
-  );
+export const MiniPlayer: React.FC<MiniPlayerProps> = ({track, isPlaying, onOpen,}) => {
+    const [isLiked, setIsLiked] = useState(false);
+
+    const handleLike = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        setIsLiked(!isLiked);
+    };
+
+    return (
+        <div className={css.miniPlayer}>
+            <img src={track.poster} alt={track.title} className={css.poster}/>
+            <div className={css.marquee}>
+                <span>{`${track.title} - ${track.artist}`}</span>
+            </div>
+            <button
+                className={css.likeButton}
+                onClick={handleLike}
+            >
+                {isLiked ? (
+                    <IoHeart size={24} className={css.likeIcon}/>
+                ) : (
+                    <IoHeartOutline size={24} className={css.likeIcon}/>
+                )}
+            </button>
+            <button className={css.playButton} onClick={onOpen}>
+                {isPlaying ? (
+                    <IoPauseCircle size={32} className={css.playIcon}/>
+                ) : (
+                    <IoPlayCircle size={32} className={css.playIcon}/>
+                )}
+            </button>
+        </div>
+    );
 };
